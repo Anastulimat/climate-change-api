f623de02b3132c04303888bf9f1e55c42bae5aa4
TULIMAT Anas OBS/CSO <anas.tulimat@orange.com>
2021-12-28 15:12:14 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const PORT = 8000;
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+// express app
+const app = express();
+
+// Variables declarations
+const articles = [];
+
+
+app.get('/', (req, res) => {
+    res.json('Welcome to my Climate Change News API');
+});
+
+/**
+ * Get news by scrapping the guardian website
+ */
+app.get('/news', (req, res) => {
+    axios.get('https://www.theguardian.com/environment/climate-crisis')
+        .then((response) => {
+            const html = response.data;
+            const $ = cheerio.load(html);
+
+            $('a:contains("climate")', html).each(function () {
+                const title = $(this).text();
+                const url = $(this).attr('href');
+                articles.push({
+                    title,
+                    url
+                });
+            });
+            res.json(articles);
+        }).catch((err) => console.log(err));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}`);
+});
