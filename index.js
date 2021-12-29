abbb59116ae5b6359adf7e8a143f7fb077185ac5
TULIMAT Anas OBS/CSO <anas.tulimat@orange.com>
2021-12-29 17:05:08 +0100

    feat: add multiple sources of news

--- index.js
+++ index.js
@@ -7,7 +7,43 @@ const cheerio = require('cheerio');
 const app = express();
 
 // Variables declarations
+const newspapers = [
+    {
+        name: 'thetimes',
+        address: 'https://www.thetimes.co.uk/environment/climate-change',
+        base: ''
+    },
+    {
+        name: 'guardian',
+        address: 'https://www.theguardian.com/environment/climate-crisis',
+        base: ''
+    },
+    {
+        name: 'telegraphe',
+        address: 'https://www.telegraph.co.uk/climate-change/',
+        base: 'https://www.telegraph.co.uk'
+    },
+]
 const articles = [];
+
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address)
+        .then((response) => {
+            const html = response.data;
+            const $ = cheerio.load(html);
+
+            $('a:contains("climate")', html).each(function () {
+                
+                const title = $(this).text();
+                const url = $(this).attr('href');
+                articles.push({
+                    title,
+                    url: newspaper.base + url,
+                    source: newspaper.name
+                });
+            });
+        }).catch((err) => console.log(err));
+});
 
 
 app.get('/', (req, res) => {
@@ -18,21 +54,7 @@ app.get('/', (req, res) => {
  * Get news by scrapping the guardian website
  */
 app.get('/news', (req, res) => {
-    axios.get('https://www.theguardian.com/environment/climate-crisis')
-        .then((response) => {
-            const html = response.data;
-            const $ = cheerio.load(html);
-
-            $('a:contains("climate")', html).each(function () {
-                const title = $(this).text();
-                const url = $(this).attr('href');
-                articles.push({
-                    title,
-                    url
-                });
-            });
-            res.json(articles);
-        }).catch((err) => console.log(err));
+    res.json(articles);
 });
 
 app.listen(PORT, () => {
